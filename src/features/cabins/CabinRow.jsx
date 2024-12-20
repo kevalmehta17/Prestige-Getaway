6e72f118af9f60d72f17be3217a6aca94ca2ff8f
Keval <kevalmehta885@gmail.com>
2024-12-20 16:54:20 +0530

    react-hot-toast Notificatio Feature Provided to App.jsx

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
 
 const TableRow = styled.div`
   display: grid;
@@ -59,13 +60,13 @@ function CabinRow({ cabin }) {
     mutationFn: deleteCabin,
 
     onSuccess: () => {
-      alert("Cabin deleted successfully");
+      toast.success("Cabin deleted successfully");
       queryClient.invalidateQueries({
         queryKey: ["cabins"],
       });
     },
     onError: (error) => {
-      alert("An error occurred: " + error.message);
+      toast.error("An error occurred: " + error.message);
       console.log(error);
     },
   });
